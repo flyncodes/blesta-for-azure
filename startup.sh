f50dbefe3bef23e4089e232ef0797b198db0cb00
Flyn <84115766+flyncodes@users.noreply.github.com>
2023-05-10 20:41:04 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,2 @@
-#Make a directory on the local disk to run the code from
-mkdir -p /usr/local/cachedapp/
-
 #Copy the contents from shared storage to a folder on local disk
 cp -R /home/site/wwwroot/inside /usr/local/cachedapp/wwwroot
