b00b2b611fc2b836a05ebd48c5f7ee5bc5bd8600
Flyn <84115766+flyncodes@users.noreply.github.com>
2023-05-10 21:26:04 +0100

    Update 30-copy-site.sh

--- docker-entrypoint.d/30-copy-site.sh
+++ docker-entrypoint.d/30-copy-site.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Copy the contents from shared storage to a folder on local disk
-cp -R /home/site/wwwroot/inside /usr/local/cachedapp/wwwroot
+cp -R /home/site/wwwroot/inside/* /usr/local/cachedapp/wwwroot
+echo "Copied contents from shared storage to local disk"
